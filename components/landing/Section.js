acfceff7cce90019ec396547012182d71850a14d
Edison Caceres <edisonyca127@gmail.com>
2025-04-04 23:00:47 -0400

    Finished hero section, but may not be responsinve to larger screens yet. Section may need to be larger.

--- components/landing/Section.js
+++ components/landing/Section.js
@@ -1,18 +1,24 @@
-import Image from "next/image";
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
 
 export default function Section() {
     return (
-            <section className="flex w-full h-3/6">
+            <section className="flex w-full h-3/6 p-15">
                 <div className="w-1/2 space-y-3">
-                    <h1 className="text-6xl font-bold max-w-2xl">Empowering Freelancers & Clients With Decentralized Connections</h1>
+                    <h1 className="text-2xl md:text-4xl lg:text-6xl font-bold max-w-2xl">Empowering Freelancers & Clients With Decentralized Connections</h1>
                     <p className="text-2xl max-w-2xl">Seamless, secure, and decentralized—connect, collaborate, and get paid without middlemen.</p>
                     <div className="flex w-96 h-11 gap-2">
-                        <button className="bg-ut-orange w-full rounded-3xl">FIND JOBS</button>
-                        <button className="border w-full rounded-3xl">FIND TALENT</button>
+                        <button className="bg-ut-orange custom-button">FIND JOBS</button>
+                        <button className="border custom-button">FIND TALENT</button>
                     </div>
                 </div>
-                <div className="bg-blue-500 w-1/2">
-                    <Image src="/images/hero.png" alt="hero" width={100} height={100} />
+                <div className="w-1/2">
+                    <DotLottieReact
+                    className="object-cover"
+                    src="https://lottie.host/f6ca150a-57b0-4958-9b01-c67f78570beb/9ns3BTbHE2.lottie"
+                    loop
+                    autoplay
+                    />
                 </div>
             </section>
     )
