028286280d090f80efe519c228a6815cec11171c
Edison Caceres <edisonyca127@gmail.com>
2025-04-06 15:17:13 -0400

    Landing page complete frontend complete. I will return in the future to make it look cleaner.

--- components/landing/Section.js
+++ components/landing/Section.js
@@ -1,22 +1,37 @@
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 
-export default function Section({reverse=false, lottie="https://lottie.host/f6ca150a-57b0-4958-9b01-c67f78570beb/9ns3BTbHE2.lottie"}) {
+export default function Section(
+    {   feature=false, 
+        lottie="https://lottie.host/f6ca150a-57b0-4958-9b01-c67f78570beb/9ns3BTbHE2.lottie",
+        title="Empowering Freelancers & Clients With Decentralized Connections",
+        description="Seamless, secure, and decentralized—connect, collaborate, and get paid without middlemen.",
+        featureTitle="CRYPTO PAYMENTS",
+        left=false
+    }
+
+) {
     return (
-            <section className={`flex w-screen ${reverse ? 'flex-row-reverse' : 'flex-row'} h-screen p-15`}>
+            <section className={`flex w-screen ${left ? 'flex-row-reverse' : 'flex-row'} h-screen p-15`}>
                 <div className="w-1/2 space-y-3">
-                    {reverse ? <h2 className="text-lg font-bold max-w-2xl">Empowering Freelancers & Clients With Decentralized Connections</h2> 
-                    :
-                     <h1 className="text-2xl md:text-4xl lg:text-6xl font-bold max-w-2xl">Empowering Freelancers & Clients With Decentralized Connections</h1>
+                    {feature ? 
+                    <>
+                        <h3 className="text-2xl font-bold">{featureTitle}</h3> 
+                        <h2 className="text-4xl font-bold max-w-2xl">{title}</h2> 
+                        <p className="text-xl max-w-2xl">{description}</p>
+                    </>
+                    : 
+                    <>
+                        <h1 className="text-2xl md:text-4xl lg:text-6xl font-bold max-w-2xl">{title}</h1>
+                        <p className="text-2xl max-w-2xl">{description}</p>
+                    </>
                     }
-                    
-                    <p className="text-2xl max-w-2xl">Seamless, secure, and decentralized—connect, collaborate, and get paid without middlemen.</p>
-                    <div className="flex w-96 h-11 gap-2">
-                        <button className="bg-ut-orange custom-button">FIND JOBS</button>
-                        <button className="border custom-button">FIND TALENT</button>
+                    <div className={`flex h-11 ${feature ? "w-64" : "w-96 gap-2"}`}>
+                        <button className={`${feature ? "bg-prussian-blue" : "bg-ut-orange"} custom-button`}>FIND JOBS</button>
+                        {feature ? null : <button className="border custom-button">FIND TALENT</button>}
                     </div>
                 </div>
-                <div className="w-8/12">
+                <div className='w-8/12'>
                     <DotLottieReact
                     className="object-cover"
                     src={lottie}
--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,20 @@ export default function Home() {
       <Navbar />
       <Section />
       <Section 
-      reverse 
-      lottie="https://lottie.host/a9a324fc-b384-4016-89e3-a09f9b00d486/yRe5R9WK8I.lottie" />
+      feature 
+      lottie="https://lottie.host/ffdfc47d-d14f-43f9-925a-0a4205870728/EFp8RcgETf.lottie" 
+      title="Get Your Project Built & Pay In Your Favorite Crypto"
+      description="SafeGig supports tokens on the Ethereum, Binance Smart Chain, TRON and Polygon networks, providing fast, efficient settlement between Customers and Freelancers anywhere in the world."
+      featureTitle="CRYPTO PAYMENTS"
+      left
+      />
+      <Section 
+      feature
+      lottie="https://lottie.host/8cb092d9-0408-4dff-83b2-6ec4264529d0/KJA1IhHZzJ.lottie" 
+      title="Trustless Agreements With Smart Contracts"
+      description="Work with confidence — all agreements are enforced by blockchain smart contracts, ensuring fair milestones, secure payments, and no third-party interference."
+      featureTitle="SMART CONTRACTS"
+      />
     </>
   );
 }
